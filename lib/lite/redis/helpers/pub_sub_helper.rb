df774d2eef21ca6c7f58d82b1fb71f1a2749aa79
Juan Gomez <j.gomez@drexed.com>
2019-07-08 17:59:00 -0400

    Rework pubsub

--- lib/lite/redis/helpers/pub_sub_helper.rb
+++ lib/lite/redis/helpers/pub_sub_helper.rb
@@ -12,24 +12,32 @@ module Lite
         client.subscribed?
       end
 
-      def subscribe(*channels, &block)
-        client.subscribe(channels, &block)
+      def subscribe(*channels, timeout: nil, &block)
+        if timeout.nil?
+          client.subscribe(*channels, &block)
+        else
+          client.subscribe_with_timeout(timeout, *channels, &block)
+        end
       end
 
       def unsubscribe(*channels)
-        client.unsubscribe(channels)
+        client.unsubscribe(*channels)
       end
 
-      def match_subscribe(*channels, &block)
-        client.psubscribe(channels, &block)
+      def match_subscribe(*channels, timeout: nil, &block)
+        if timeout.nil?
+          client.psubscribe(*channels, &block)
+        else
+          client.psubscribe_with_timeout(timeout, *channels, &block)
+        end
       end
 
       def match_unsubscribe(*channels)
-        client.punsubscribe(channels)
+        client.punsubscribe(*channels)
       end
 
       def state(command, *args)
-        client.pubsub(command, args)
+        client.pubsub(command, *args)
       end
 
     end
