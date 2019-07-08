816687db57130c134fa294f5aaf2b1169df6912e
Juan Gomez <j.gomez@drexed.com>
2019-07-08 14:19:51 -0400

    Init commit

---
+++ spec/lite/redis_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Lite::Redis do
+  it "has a version number" do
+    expect(Lite::Redis::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
